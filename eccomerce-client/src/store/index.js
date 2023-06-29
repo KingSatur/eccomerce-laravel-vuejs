217237119b3457db66f3a9c2d63ff1a62a598949
Juan Campillo <juancawool@outlook.com>
2023-06-28 20:53:03 -0500

    feat: creating client application with vite and vue

---
+++ eccomerce-client/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state: {
+        test: 'value'
+    },
+    getters: {},
+    actions: {},
+    mutations: {}
+})
+
+export default store
